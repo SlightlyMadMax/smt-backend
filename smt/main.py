6790f4b7cb46cf5fac4da60c59293d8835a2f674
SlightlyMadMax <makcandreev2001@mail.ru>
2025-02-25 18:09:46 +0300

    add templates for testing

--- smt/main.py
+++ smt/main.py
@@ -1,9 +1,12 @@
 import uvicorn
+
 from fastapi import FastAPI, APIRouter
 from starlette.middleware.cors import CORSMiddleware
 from starlette.middleware.sessions import SessionMiddleware
+from starlette.staticfiles import StaticFiles
 
 from api.v1.routes.auth import auth
+from api.v1.routes.frontend import home
 from smt.core.config import settings
 
 
@@ -17,10 +20,14 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+app.mount("/static", StaticFiles(directory="/code/smt/static"), name="static")
+
+
 api_router = APIRouter(prefix=f"/api/{settings.API_VERSION}")
 
 api_router.include_router(auth.router, prefix="/auth", tags=["Auth"])
 app.include_router(api_router)
+app.include_router(home.router)
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", reload=True)
