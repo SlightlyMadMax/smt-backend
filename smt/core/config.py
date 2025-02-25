39015b1300193027624c21ed62d34d2595538f3e
SlightlyMadMax <makcandreev2001@mail.ru>
2025-02-25 16:21:52 +0300

    steam openid auth

--- smt/core/config.py
+++ smt/core/config.py
@@ -1,15 +1,28 @@
-from pydantic import computed_field
+from pydantic import computed_field, HttpUrl
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
     PROJECT_NAME: str = "Steam Market Trader"
+    BASE_URL: HttpUrl
+    DEBUG: bool
+    SESSION_SECRET_KEY: str
+    API_VERSION: str
     PORT: int = 8000
+
     DB_USER: str
     DB_PASSWORD: str
     DB_NAME: str
     DB_HOST: str
     DB_PORT: int = 5432
+
+    STEAM_API_KEY: str
+    STEAM_OPENID_URL: HttpUrl
+
+    @computed_field
+    @property
+    def STEAM_RETURN_URL(self) -> HttpUrl:
+        return HttpUrl(f"{self.BASE_URL}api/{self.API_VERSION}/auth/steam/callback")
 
     @computed_field
     @property
--- smt/main.py
+++ smt/main.py
@@ -1,13 +1,26 @@
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, APIRouter
+from starlette.middleware.cors import CORSMiddleware
+from starlette.middleware.sessions import SessionMiddleware
 
-app = FastAPI()
+from api.v1.routes.auth import auth
+from smt.core.config import settings
 
 
-@app.get("/")
-def main():
-    return "Hello, world!"
+app = FastAPI(title=settings.PROJECT_NAME, debug=settings.DEBUG)
+app.add_middleware(SessionMiddleware, secret_key=settings.SESSION_SECRET_KEY)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Change this to specific domains in production
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
+api_router = APIRouter(prefix=f"/api/{settings.API_VERSION}")
+
+api_router.include_router(auth.router, prefix="/auth", tags=["Auth"])
+app.include_router(api_router)
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", reload=True)
