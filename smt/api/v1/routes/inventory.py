504da0f944f6607fd912bd7dfb4916e510b2f276
SlightlyMadMax <makcandreev2001@mail.ru>
2025-05-31 00:24:38 +0300

    update read_inventory; add update_inventory

--- smt/api/v1/routes/inventory.py
+++ smt/api/v1/routes/inventory.py
@@ -4,7 +4,7 @@ from fastapi import APIRouter, Depends, Query
 
 from smt.schemes.inventory import GAME_MAP, GameName, InventoryItem
 from smt.services.steam import SteamService
-
+from smt.utils.steam import transform_inventory_item
 
 router = APIRouter(prefix="/inventory", tags=["inventory"])
 
@@ -15,17 +15,18 @@ async def read_inventory(
     steam: SteamService = Depends(),
 ):
     game_option = GAME_MAP[game]
-    inventory = steam.get_inventory(game=game_option)
+    raw_inventory = steam.get_inventory(game=game_option)
+    items = [transform_inventory_item(item) for item in raw_inventory.values()]
+    return items
 
-    return [
-        InventoryItem(
-            id=data.get("assetid", assetid),
-            name=data.get("name", ""),
-            market_hash_name=data.get("market_hash_name", ""),
-            tradable=int(data.get("tradable", 0)),
-            marketable=int(data.get("marketable", 0)),
-            amount=data.get("amount", "1"),
-            icon_url=f"https://steamcommunity-a.akamaihd.net/economy/image/{data.get('icon_url', '')}",
-        )
-        for assetid, data in inventory.items()
-    ]
+
+@router.put("/update-inventory")
+async def update_inventory(
+    game: GameName = Query(...), steam: SteamService = Depends()
+):
+    game_option = GAME_MAP[game]
+    raw_inventory = steam.get_inventory(game=game_option)
+    items = [transform_inventory_item(item) for item in raw_inventory.values()]
+
+    # await save_inventory_cache(game_option.app_id, items)
+    return {"message": "Inventory updated", "count": len(items)}
