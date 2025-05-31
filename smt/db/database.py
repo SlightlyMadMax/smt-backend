1fbef983f411709d5b192e55dc5e541edb05f951
SlightlyMadMax <makcandreev2001@mail.ru>
2025-05-31 18:54:13 +0300

    add TimeStampedModel

--- smt/db/database.py
+++ smt/db/database.py
@@ -1,12 +1,27 @@
+from datetime import datetime
+
+from sqlalchemy import DateTime, func
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
-from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
 from smt.main import settings
 
 
-engine = create_async_engine(settings.DATABASE_URI)
-async_session_maker = async_sessionmaker(engine, class_=AsyncSession)
+engine = create_async_engine(settings.DATABASE_URI, pool_size=20, max_overflow=10, pool_pre_ping=True)
+async_session_maker = async_sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
 
 
 class Base(DeclarativeBase):
     pass
+
+
+class TimeStampedModel(Base):
+    """
+    Abstract model that adds automatic timestamp fields
+    to any model that inherits from it.
+    """
+
+    __abstract__ = True
+
+    created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now(), nullable=False)
+    updated_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), onupdate=func.now(), nullable=True)
