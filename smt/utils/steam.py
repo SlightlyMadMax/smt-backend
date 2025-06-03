9cd2760bf005a2db1e2fa554019021f945ffd8d8
SlightlyMadMax <makcandreev2001@mail.ru>
2025-06-03 20:30:03 +0300

    fix amount not being converted to int

--- smt/utils/steam.py
+++ smt/utils/steam.py
@@ -3,8 +3,8 @@ def transform_inventory_item(item: dict) -> dict:
         "id": item["id"],
         "name": item["name"],
         "market_hash_name": item["market_hash_name"],
-        "tradable": item["tradable"],
-        "marketable": item["marketable"],
-        "amount": item["amount"],
+        "tradable": int(item.get("tradable", 0)),
+        "marketable": int(item.get("marketable", 0)),
+        "amount": int(item.get("amount", 1)),
         "icon_url": f"https://steamcommunity-a.akamaihd.net/economy/image/{item['icon_url']}",
     }
