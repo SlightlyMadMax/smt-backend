13dc1da9d4a54bf6fde5482ec11464d60bdd4a39
SlightlyMadMax <makcandreev2001@mail.ru>
2025-06-03 18:01:35 +0300

    add stats schemes

---
+++ smt/schemes/stats.py
@@ -0,0 +1,19 @@
+from datetime import datetime
+
+from pydantic import BaseModel, ConfigDict
+
+
+class ItemStatBase(BaseModel):
+    market_hash_name: str
+    recorded_at: datetime
+    price: float
+    volume: int
+
+
+class ItemStatCreate(ItemStatBase):
+    model_config = ConfigDict(from_attributes=False)
+
+
+class ItemStat(ItemStatBase):
+    id: int
+    model_config = ConfigDict(from_attributes=True)
