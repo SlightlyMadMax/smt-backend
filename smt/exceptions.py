3e891795a9c601c2a3f595bf09dc94c2376bedb0
SlightlyMadMax <makcandreev2001@mail.ru>
2026-08-30 15:54:57 +0300

    add domain exception hierarchy

---
+++ smt/exceptions.py
@@ -0,0 +1,18 @@
+class SMTError(Exception):
+    """Base class for all application specific errors."""
+
+
+class SteamOperationFailed(SMTError):
+    """Steam accepted the request but reported a failure."""
+
+
+class BuyOrderFailed(SteamOperationFailed):
+    pass
+
+
+class SellOrderFailed(SteamOperationFailed):
+    pass
+
+
+class ListingNotResolved(SteamOperationFailed):
+    """A sell order was placed but its listing id could not be found yet."""
